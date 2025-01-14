cbe5bfd61a18ba587f01a9ae0657fdb57d32e610
Andrea ~ <123588571+andreanun@users.noreply.github.com>
2025-01-14 01:05:42 +0000

    completed 1,2 calculator exercise

---
+++ demo/src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+  
+  public int sum(int x, int y) {
+    return x + y;
+  }
+
+  public int subtract(int x, int y) {
+    return x - y;
+  }
+
+  public int multiply(int x, int y){
+    return x * y;
+  }
+
+  public int divide(int x, int y) {
+    if (y == 0 ) { return -1;}
+    return x / y;
+  }
+}
---
+++ demo/src/test/java/CalculatorTest.java
@@ -0,0 +1,57 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+  Calculator calculator = new Calculator();
+
+  @Test
+  public void testSum() {
+    int expected = 3;
+
+    int actual = calculator.sum(1, 2);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testSubtract() {
+    int expected = -1;
+    int actual = calculator.subtract(1, 2);
+
+    assertEquals(expected, actual);
+
+  }
+
+  @Test
+  public void testMultiply() {
+    int expected = 12;
+    int actual = calculator.multiply(3,4);
+
+    assertEquals(expected, actual);
+
+  }
+
+  @Test
+  public void testDivide() {
+    int expected = 3;
+    int actual = calculator.divide(12, 4);
+
+    assertEquals(expected, actual);
+    
+  }
+
+  @Test
+  public void testDivideByZero() {
+    int expected = -1;
+    int actual = calculator.divide(12, 0);
+
+    assertEquals(expected, actual);
+
+    
+  }
+
+
+
+}
